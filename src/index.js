816c655c23a81cf97dcaafaca4d63985b2415a46
Femclark <femclark@gmail.com>
2023-01-12 23:45:44 -0300

    Primera prueba en marcha

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+
+// Inicializaciones
+const app = express();
+
+// Configuraciones
+app.set('port', process.env.PORT || 4000);
+
+// Middlewares
+app.use(morgan('dev'));
+
+// Variables Globales
+
+
+//rutas
+
+// Public
+
+// Start
+app.listen(app.get('port'), () => {
+    console.log('Server is in port', app.get('port'));
+  });
