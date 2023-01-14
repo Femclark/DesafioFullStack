cd8869c3cbba49b16724ca4447b1937f2114dabf
Femclark <femclark@gmail.com>
2023-01-14 10:10:08 -0300

    - Hasta aquí he insertado y mostrado los alumnos (Estructura de la página)

--- src/routers/links.js
+++ src/routers/links.js
@@ -27,4 +27,41 @@ router.post('/add', async(req, res) => {
     res.send('Recivido');
 });
 
+// http://localhost:4000/links
+router.get('/', async (req, res) => {
+    //const links = await pool.query('SELECT * FROM alumnos WHERE user_id = ?', [req.user.id]);
+    const links = await pool.query('SELECT * FROM alumnos ');
+    //console.log(links)
+    //res.send('ok')
+    res.render('links/list', { links });
+});
+
+router.get('/delete/:id', async (req, res) => {
+    //console.log(req.params);
+    const { id } = req.params;
+    await pool.query('DELETE FROM alumnos WHERE ID = ?', [id]);
+    req.flash('success', 'Alumno Removed Successfully');
+    res.redirect('/links');
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const links = await pool.query('SELECT * FROM alumnos WHERE id = ?', [id]);
+    console.log(links);
+    res.render('links/edit', {link: links[0]});
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, description, url} = req.body; 
+    const newLink = {
+        title,
+        description,
+        url
+    };
+    await pool.query('UPDATE alumnos set ? WHERE id = ?', [newLink, id]);
+    req.flash('success', 'alumno Updated Successfully');
+    res.redirect('/links');
+});
+
 module.exports = router;
