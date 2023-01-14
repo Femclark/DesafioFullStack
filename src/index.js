85249a66de0fd99b43a35aabd4044a35e2cbf4b4
Femclark <femclark@gmail.com>
2023-01-14 09:19:16 -0300

    - insertando valor prueba por formulario add (add: formulario para insertar alumnos)

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const app = express();
 // Configuraciones
 // Settings
 app.set('port', process.env.PORT || 4000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views/'));
 app.engine('.hbs', exphbs.engine({
   defaultLayout: 'main',
   layoutsDir: path.join(app.get('views'), 'layouts'),
--- src/routers/links.js
+++ src/routers/links.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const { route } = require('.');
+const pool = require('../database');
 const router = express.Router();
 
 const dbpool=require('../database');
+
+// http://localhost:4000/links/add
+router.get('/add', (req, res) => {
+    res.render('links/add');
+    //res.send('form');
+});
+
+router.post('/add', async(req, res) => {
+    //res.render('links/add');
+    //console.log(req.body);
+    const { title, url, description } = req.body; // objeto body lo transformo a lista
+    const newLink = {
+        nombre:title,
+        password:url,
+        apellido:description,
+        rol:1,
+        idCarrera:1
+    };
+    //await pool.query('INSERT INTO alumnos set ?', [newLink]);
+    console.log(newLink);
+    res.send('Recivido');
+});
 
 module.exports = router;
