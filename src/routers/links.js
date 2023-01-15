bb20e66675e04d95cf2258191331bfb380351b72
Femclark <femclark@gmail.com>
2023-01-15 18:10:07 -0300

    - Se realizan las operaciones solicitadas del admin y crud

--- src/routers/links.js
+++ src/routers/links.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const pool = require('../database');// Conexion a BD
 const { isLoggedIn } = require('../lib/auth');// llamo a mi libreria de autentificacion
+const helpers = require('../lib/helpers');// requiero el encriptador 
 
 // http://localhost:4000/links/add
 router.get('/add', (req, res) => {
@@ -12,17 +13,18 @@ router.get('/add', (req, res) => {
 
 router.post('/add', async(req, res) => {
     //res.render('links/add');
-    //console.log(req.body);
-    const { title, url, description } = req.body; // objeto body lo transformo a lista
-    const newLink = {
-        nombre:title,
-        password:url,
-        apellido:description,
-        rol:1,
-        idCarrera:1,
-        idAlumnos: req.user.id
+    console.log(req.body);
+    const { nombre, apellido,carrera, password } = req.body; // objeto body lo transformo a lista
+    const newUser = {
+        nombre,
+        apellido,
+        password,
+        rol:1, // rol usuario
+        idCarrera:carrera,
     };
-    await pool.query('INSERT INTO alumnos set ?', [newLink]);
+    //console.log(newUser);
+    newUser.password = await helpers.encryptPassword(password);// Encripto la password
+    await pool.query('INSERT INTO alumnos set ?', [newUser]); 
     req.flash('success', 'Link Saved Successfully');
     res.redirect('/links');
 });
@@ -31,14 +33,8 @@ router.post('/add', async(req, res) => {
 router.get('/', isLoggedIn, async (req, res) => {
     //console.log(req.user.rol);
     if(req.user.rol==0){
-        console.log('paso por aqui con id y rol admin');
-        console.log(req.user.idAlumno)
-        console.log(req.user.rol)
         var links = await pool.query('SELECT * FROM alumnos ');
     }else{
-        console.log('paso por aqui con id y rol user');
-        console.log(req.user.idAlumno)
-        console.log(req.user.rol)
         var links = await pool.query('SELECT * FROM alumnos WHERE idAlumno = ?', [req.user.idAlumno]);
     }   
     //const links = await pool.query('SELECT * FROM alumnos ');
@@ -49,27 +45,29 @@ router.get('/delete/:id', async (req, res) => {
     //console.log(req.params);
     const { id } = req.params;
     await pool.query('DELETE FROM alumnos WHERE idAlumno = ?', [id]);
-    req.flash('success', 'Alumno Removed Successfully');
+    req.flash('success', 'Alumno Eliminado Exitosamente');
     res.redirect('/links');
 });
 
 router.get('/edit/:id', async (req, res) => {
     const { id } = req.params;
     const links = await pool.query('SELECT * FROM alumnos WHERE idAlumno = ?', [id]);
-    console.log(links);
-    res.render('links/edit', {link: links[0]});
+    let nameCarrera=['ICI','ICQ','IECI'];
+    nameCarrera=nameCarrera[links[0].idCarrera];
+    //carreras= nameCarrera.filter();
+    res.render('links/edit', {link: links[0], nameCarrera});
 });
 
 router.post('/edit/:id', async (req, res) => {
     const { id } = req.params;
-    const { title, description, url} = req.body; 
+    const { nombre, apellido, carrera} = req.body; 
     const newLink = {
-        title,
-        description,
-        url
+        nombre,
+        apellido,
+        idCarrera:carrera
     };
     await pool.query('UPDATE alumnos set ? WHERE idAlumno = ?', [newLink, id]);
-    req.flash('success', 'alumno Updated Successfully');
+    req.flash('success', 'alumno actualizado satisfactoriamente');
     res.redirect('/links');
 });
 
